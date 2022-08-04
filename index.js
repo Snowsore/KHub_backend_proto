14b2fb86927ceeb888a018eb763aa4abae64998e
Snowsore <snow_sore@hotmail.com>
2022-08-04 14:23:52 +0800

    Enable cors

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const routerRoot = require("./router");
 
+const corsOptions = {
+  origin: "http://localhost.com",
+};
+
+app.use(cors(corsOptions));
 app.use("/", routerRoot);
 
 app.listen("8081", () => {
