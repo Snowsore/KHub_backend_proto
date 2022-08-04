5a23eeacd1213c752caa24b0059c06cd8a620e82
Snowsore <snow_sore@hotmail.com>
2022-08-04 14:43:41 +0800

    Add test message to default request

--- router/index.js
+++ router/index.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const routerUser = require("./user");
 const routerFile = require("./file");
 
+router.get("/", (req, res) => {
+  res.json({ msg: "Welcome ;)" });
+});
+
 router.use("/user", routerUser);
 router.use("/file", routerFile);
 
